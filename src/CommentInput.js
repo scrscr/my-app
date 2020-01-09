776ac92c1e82b5826932912dd75689f2f98db23b
scrscr <384917634@qq.com>
2020-01-09 19:10:34 +0800

    增加评论发布时间

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,9 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from "react"
+class CommentInput extends Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      content: ''
+    }
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  }
+  componentWillMount(){
+    this._loadUsername()
+  }
+  componentDidMount(){
+    this.textarea.focus()
+  }
+  _loadUsername(){
+    const username=localStorage.getItem('username')
+    if(username){
+      this.setState({username})
+    }
+  }
+_saveUsername(username){
+  localStorage.setItem('username',username)
+}
+handleUsernameBlur(event){
+  this._saveUsername(event.target.value)
+}
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+  handleContentChange(event){
+    this.setState({
+      content:event.target.value
+    })
+  }
+ handleSubmit(event){
+   if(this.props.onSubmit){
+     this.props.onSubmit({username:this.state.username,
+      content:this.state.content,
+      createdTime:+new Date()
+    })
+   }
+   this.setState({content:''})
+ }
+  render() {
+    return (
+      <div className="comment-input">
+        <div className="comment-field">
+          <span className="comment-field-name">用户名：</span>
+          <div className="comment-field-input">
+            <input value={this.state.username} onBlur={this.handleUsernameBlur.bind(this)} onChange={this.handleUsernameChange.bind(this)} />
+          </div>
+        </div>
+        <div className="" comment-field>
+          <span className="comment-field-name">评论内容：</span>
+          <div className="comment-field-input">
+            <textarea ref={(textarea)=>this.textarea=textarea} value={this.state.content} onChange={this.handleContentChange.bind(this)} />
+          </div>
+        </div>
+        <div className="comment-field-button">
+          <button onClick={this.handleSubmit.bind(this)}>发布</button>
+        </div>
+      </div>
+    )
+  }
+}
+export default CommentInput
